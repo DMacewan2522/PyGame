d4b8f7e1f48dd51a617b40d4d03fdf426e8cbf37
DMacewan <86538969+DMacewan2522@users.noreply.github.com>
2022-05-14 01:51:15 +0100

    Idle Animation, Additional Commenting
    Idle Animation, Additional Commenting

--- ScrollingShooter.py
+++ ScrollingShooter.py
@@ -29,15 +29,33 @@ class PlayerCharacter(pygame.sprite.Sprite):
         self.movespeed = movespeed
         self.movedirection = 1
         self.directionChange = False
+        self.animationList = []
+        self.animationIndex = 0
+        self.timeTracker = pygame.time.get_ticks()
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('Gunner_Idle.gif')  #Assign the player's sprite an image
-        #Change image size according to game size
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), (int(img.get_height() * scale))))
+        for i in range(4):
+            img = pygame.image.load(f'assets/Player_Idle_{i}.png')  #Assign the player's sprite an image
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), (int(img.get_height() * scale)))) #Change image size
+            self.animationList.append(img)
+        self.image = self.animationList[self.animationIndex]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
     #Function to place player onto the screen
     def createPlayer(self):
         screen.blit(pygame.transform.flip(self.image, self.directionChange, False),  self.rect)
+
+    #Function that handles player animations on a timer
+    def handleAnimations(self):
+        #Timer for animation reset
+        animationTimer = 300
+        self.image = self.animationList[self.animationIndex]
+        if pygame.time.get_ticks() - self.timeTracker > animationTimer:
+            self.animationIndex += 1        #Increase the list index by one each pass of the loop
+            self.timeTracker = pygame.time.get_ticks()      #Reset timeTracker each pass of the loop
+        if self.animationIndex >= len(self.animationList):      #Once the index reaches the length of the list...
+            self.animationIndex = 0     #Reset the animation index to reset the animation
+
     #Function that handles player movements
     def playerMovement(self, moveLeft, moveRight):
         twox = 0
@@ -46,15 +64,21 @@ class PlayerCharacter(pygame.sprite.Sprite):
             twox = -self.movespeed
             self.directionChange = True
             self.direction = -1
-        if moveRight:
+        if moveRight:                           #Detects when the player is moving left to notify the game on when to flip the sprite
             twox = self.movespeed
             self.directionChange = False
             self.direction = 1
         self.rect.x += twox
         self.rect.y += twoy
 
+
+
+
 #Create player
-playerModel = PlayerCharacter(200, 200, 3, 5)
+playerModel = PlayerCharacter(200, 200, 2, 5)
+
+
+
 
 #Game Loop
 run = True
@@ -62,6 +86,7 @@ while run:
     #Game Initializations
     clock.tick(FrameRate)
     createBackground()
+    playerModel.handleAnimations()
     playerModel.createPlayer()
     playerModel.playerMovement(moveLeft, moveRight)
 
@@ -71,17 +96,17 @@ while run:
             run = False
         #Player Inputs
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a:     #Move left
                 moveLeft = True
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d:     #Move right
                 moveRight = True
-            if event.key == pygame.K_ESCAPE:
+            if event.key == pygame.K_ESCAPE:    #Close the game
                 run = False
         #Stop Movements
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a:     #Stop left movement
                 moveLeft = False
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d:     #Stop right movement
                 moveRight = False
 
     #Keep game display updated with new information
